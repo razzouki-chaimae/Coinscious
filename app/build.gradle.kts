94b6b3772979d230e7da082351d4e6956ca44c2c
RAZZOUKI Chaimae <chaimaerazzouki.2017@gmail.com>
2026-09-01 10:11:01 +0100

    Add GreetingPreview selection state, update Gradle settings and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,7 @@ dependencies {
     implementation(libs.kotlinx.datetime)
     implementation(libs.androidx.compose.runtime)
     implementation(libs.androidx.compose.foundation.layout)
+    implementation(libs.androidx.material3)
     // Testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
